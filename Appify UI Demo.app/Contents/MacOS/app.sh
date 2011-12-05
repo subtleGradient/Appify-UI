3ba7bf7f27df2ec348cdefbb69c24d6e79438c49
Thomas Aylott <oblivious+git@subtlegradient.com>
2011-12-05 10:36:13 -0800

    pass the url

--- Appify UI Demo.app/Contents/MacOS/app.sh
+++ Appify UI Demo.app/Contents/MacOS/app.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# launch the cocoa app
 cd "$(dirname "$0")"
-./apache-callback-mac
+
+# launch the cocoa app
+./apache-callback-mac -url "file://$(dirname "$0")/../Resources/index.html"
