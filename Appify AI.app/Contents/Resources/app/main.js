3fda5a9d332f767c54463fc0bcc916ba09a2e36d
Tom Aylott <oblivious@subtlegradient.com>
2023-04-01 19:48:48 -0400

    let's add an API form
    let the user type stuff into a box. when they hit enter it should POST that text to the server, transform it in some way, then return the results back to the client. The client will then render the results into another box

--- Appify AI.app/Contents/Resources/app/main.js
+++ Appify AI.app/Contents/Resources/app/main.js
@@ -1,34 +1,82 @@
+function html(strings, ...values) {
+  return strings
+    .map((string, index) => {
+      let value = values[index]
+      if (value === undefined) return string
+      if (typeof value === "function") value = value()
+      return string + value
+    })
+    .join("")
+}
 const fs = require("fs")
 const serverModTime = fs.statSync(__filename).mtime
 const serverStartTime = new Date()
 function indexPage() {
-  return `
-<!doctype html>
-<meta charset=utf-8>
-<title>Hello from Node!</title>
+  return html`
+    <!DOCTYPE html>
+    <meta charset="utf-8" />
+    <title>Hello from Node!</title>
 
-<h1>Hello from Node!</h1>
-<p>
-  Edit this app
-  <input type="text" value="${__filename}" readonly style="width:80%">
-</p>
+    <h1>Hello from Node!</h1>
+    <p>
+      Edit this app
+      <input type="text" value="${__filename}" readonly style="width:80%" />
+    </p>
 
-<li>Modified at: ${serverModTime}
-<li>Started at: ${serverStartTime}
-<li>Loaded at: ${new Date()}
+    <form id="apiForm">
+      <label for="inputText">Type something:</label>
+      <input type="text" id="inputText" />
+      <button type="submit">Send to server</button>
+    </form>
+    <div id="outputBox">
+      <h3>Server response:</h3>
+      <pre id="outputText"></pre>
+    </div>
 
-<h3>use fetch to load JSON when I click this button</h3>
-<script> const loadJson = () => fetch("/index.json").then(r => r.json()).then(j => document.querySelector("#json").innerText = JSON.stringify(j, null, 2)) </script>
-<button onclick="loadJson()">Load JSON</button>
-<pre id="json"></pre>
+    <li>
+      Address:
+      <script>
+        document.write(location)
+      </script>
+    </li>
+    <li>Modified at: ${serverModTime}</li>
+    <li>Started at: ${serverStartTime}</li>
+    <li>
+      Loaded at: ${new Date()}
 
-<form action="/kill" method="post">
-  <button type="submit">Kill the server</button>
-</form>
-`
+      <h3>use fetch to load JSON when I click this button: <button onclick="loadJson()">Load JSON</button></h3>
+      <pre id="json"></pre>
+      <script>
+        const loadJson = () =>
+          fetch("/index.json")
+            .then(r => r.json())
+            .then(j => (document.querySelector("#json").innerText = JSON.stringify(j, null, 2)))
+      </script>
+
+      <form action="/kill" method="post">Don't <button type="submit">Kill the server</button></form>
+    </li>
+
+    <script>
+      document.querySelector("#apiForm").addEventListener("submit", async e => {
+        e.preventDefault()
+
+        const inputText = document.querySelector("#inputText").value
+        const response = await fetch("/api", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: inputText }),
+        })
+
+        const transformedText = await response.json()
+        document.querySelector("#outputText").innerText = transformedText.text
+      })
+    </script>
+  `
 }
 
-const webviewServer = require("./lib/http-webview").create((request, response) => {
+const router = (request, response) => {
   if (request.url === "/") request.url = "/index.html"
   if (request.url === "/index.html") {
     response.writeHead(200, { "Content-Type": "text/html" })
@@ -48,12 +96,30 @@ const webviewServer = require("./lib/http-webview").create((request, response) =
     response.writeHead(200, { "Content-Type": "text/html" })
     response.write("Killing server...")
     response.end()
-    setTimeout(() => {
-      webviewServer.close()
-    }, 0)
+    setTimeout(() => webviewServer.close(), 0)
+    return
+  }
+
+  if (request.url === "/api" && request.method === "POST") {
+    let body = ""
+    request.on("data", chunk => {
+      body += chunk
+    })
+
+    request.on("end", async () => {
+      const { text } = JSON.parse(body)
+      const transformedText = text.split("").reverse().join("")
+
+      response.writeHead(200, { "Content-Type": "application/json" })
+      response.write(JSON.stringify({ text: transformedText }))
+      response.end()
+    })
+
     return
   }
 
   response.writeHead(404)
   response.end(`Not found: ${request.url}`)
-})
+}
+
+const webviewServer = require("./lib/http-webview").create(router)
