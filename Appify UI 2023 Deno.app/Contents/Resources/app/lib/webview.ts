38fb54f260184adb606358c35ba5ee42b4b0a7b0
Tom Aylott <oblivious@subtlegradient.com>
2024-05-10 20:25:31 -0400

    deno tweaks

--- Appify UI 2023 Deno.app/Contents/Resources/app/lib/webview.ts
+++ Appify UI 2023 Deno.app/Contents/Resources/app/lib/webview.ts
@@ -1,22 +1,26 @@
 const __filename = decodeURIComponent(new URL(import.meta.url).pathname)
 const __dirname = __filename.slice(0, __filename.lastIndexOf("/"))
 
-const WEBVIEW_BIN = `${__dirname}/../../../MacOS/Appify UI 23.app/Contents/MacOS/Appify UI 23`;
+const [APP_ROOT] = __dirname.split("/Contents/")
+
+const WEBVIEW_BIN = `${APP_ROOT}/Contents/MacOS/Appify UI 23.app/Contents/MacOS/Appify UI 23`
 
 export function open(url: URL | string) {
-  if (url.toString().startsWith("/")) url = `file://${url}`;
-  if (typeof url === "string") url = new URL(url);
+  if (url.toString().startsWith("/")) url = `file://${url}`
+  if (typeof url === "string") url = new URL(url)
+
+  console.log(__filename, "open", url.href)
 
   const child = Deno.run({
     cmd: [WEBVIEW_BIN, "--url", url.href],
     stdout: "inherit",
     stderr: "inherit",
-  });
+  })
 
   // wait for process to exit
-  child.status().then(status => console.log({ status }));
+  child.status().then(status => console.log({ status }))
 
-  return child;
+  return child
 }
 
 if (import.meta.main) open(`${__dirname}/webview.html`)
