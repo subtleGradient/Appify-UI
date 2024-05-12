1cc4dfdf608b5ae9750635518f6f4413f90deeb9
Tom Aylott <oblivious@subtlegradient.com>
2024-05-12 09:46:18 -0400

    chore: Add Info.plist and build script for HelloWorldApp

--- HelloWorldApp/Package.swift
+++ HelloWorldApp/Package.swift
@@ -1,14 +1,23 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "HelloWorldApp",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .executable(
+            name: "HelloWorldApp",
+            targets: ["HelloWorldApp"]),
+    ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "HelloWorldApp"),
+        .target(
+            name: "HelloWorldApp",
+            dependencies: [],
+            path: "Sources"
+        )
     ]
 )
