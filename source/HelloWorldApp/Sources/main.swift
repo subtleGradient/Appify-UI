8f9072c0e07604ec367e355b1eafd198185c8bda
Tom Aylott <oblivious@subtlegradient.com>
2024-05-18 17:08:57 -0400

    feat: Add Cocoa application functionality to HelloWorldApp

--- source/HelloWorldApp/Sources/main.swift
+++ source/HelloWorldApp/Sources/main.swift
@@ -1,4 +1,31 @@
 // The Swift Programming Language
-// https://docs.swift.org/swift-book
+// https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
 
-print("Hello, world!")
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  var window: NSWindow!
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Create the main application window
+    let screenSize = NSScreen.main!.frame
+    window = NSWindow(
+      contentRect: NSRect(
+        x: screenSize.midX - 200, y: screenSize.midY - 100, width: 400, height: 200),
+      styleMask: [.titled, .closable, .resizable, .miniaturizable],
+      backing: .buffered, defer: false)
+    window.title = "HelloWorldApp"
+    window.makeKeyAndOrderFront(nil)
+
+    // Create a label and add it to the window
+    let label = NSTextField(labelWithString: "Hello, world!")
+    label.frame = NSRect(x: 100, y: 80, width: 200, height: 40)
+    label.alignment = .center
+    window.contentView?.addSubview(label)
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+}
